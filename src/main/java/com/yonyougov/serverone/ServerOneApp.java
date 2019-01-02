e239ad89665fe3eaed6a5dc0059083ff8663ceee
wangpengz <wangpengz@yonyou.com>
2019-01-02 17:49:36 +0800

    init

---
+++ src/main/java/com/yonyougov/serverone/ServerOneApp.java
@@ -0,0 +1,13 @@
+package com.yonyougov.serverone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerOneApp {
+	
+    public static void main(String[] args) {
+    	SpringApplication.run(ServerOneApp.class, args);
+    }
+
+}
